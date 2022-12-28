fdc75225a74db7fb21a14cc8369ddebb684e1913
akamathe <advaitkamathe@gmail.com>
2022-12-28 13:14:47 +0530

    Adding OptionalDemo

---
+++ src/com/advaitkamathe/OptionalDemo.java
@@ -0,0 +1,26 @@
+package com.advaitkamathe;
+
+import com.advaitkamathe.MapAndFlatMap.Customer;
+import java.util.Arrays;
+import java.util.Optional;
+
+public class OptionalDemo {
+
+  // A container object which may or may not contain a non-null value
+  // If a value is present, isPresent() will return true and get() will return the value
+
+  public static void main(String[] args) {
+
+    Customer customer = new Customer(101, "John Doe", null, Arrays.asList("9876543210", "8976543210"));
+
+    Optional<Object> emptyOptional = Optional.empty();
+    System.out.println(emptyOptional);
+
+    // Optional<String> emailOptional = Optional.of(customer.getEmail()); // Gives NullPointerException
+
+    // ofNullable() -> returns empty() Optional object if null else returns value
+    Optional<String> emailOptional = Optional.ofNullable(customer.getEmail());
+    System.out.println(emailOptional);
+
+  }
+}
